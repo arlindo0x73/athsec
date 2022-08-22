5e2ea17afbc960d5a34402b99f7ccf323a065af6
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-22 10:16:53 +0000

    Update index.js
    Terminado...

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -10,8 +10,7 @@ const Menu = () => {
     <header className={menu.container}>
       <div>
         <div className={menu.fav}>
-          <link href="/home"><img src="/Icons/fav.png" style={{ cursor: "pointer", width: 100 }} /></link>
-
+          <img src="/Icons/fav.png" style={{ cursor: "pointer", width: 100 }} />
         </div>
       </div>
 
