b8b19abe41f4cb827f39babc45b746bae10f6ccc
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-24 01:11:00 +0000

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,8 +4,7 @@ const Header = ({ title }) =>{
   return (
     <Head>
       <meta name="description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Detecção e Resposta Gerenciadas, Avaliação de Segurança e Design de Arquitetura de Segurança" />
-      <meta name="keywords" content="Cyber Security, MSSP, MDR, ATHSec, Penetration Testing, Security Assements, Cloud, Enterprise, Solution">
-      <meta name="author" content="ATHSec">
+      <meta name="keywords" content="Cyber Security, MSSP, MDR, SOC, CSIRT">
       <link rel="shortcut icon" href="/Icons/índice.jpg" />
       <title>{title}</title>
     </Head>
