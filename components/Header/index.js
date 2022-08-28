beee0d589576a2e77c2e8d1412bbce4ee3fe6805
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-28 20:06:53 +0000

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,27 +7,10 @@ const Header = ({ title }) =>{
       <title>{title}</title>
       <link rel="shortcut icon" href="/Icons/índice.jpg" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    
-    </Head>
-    
-    <Head>
-    
-      <meta name="title" content="ATHSec">
       <meta name="description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
 
-      <meta property="og:type" content="website">
-      <meta property="og:url" content="https://athsec.org/">
-      <meta property="og:title" content="ATHSec">
-      <meta property="og:description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
-      <meta property="og:image" content="/Icons/ATHSec-HomePageMeta.jpg">
-
-      <meta property="twitter:card" content="summary_large_image">
-      <meta property="twitter:url" content="https://athsec.org/">
-      <meta property="twitter:title" content="ATHSec">
-      <meta property="twitter:description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
-      <meta property="twitter:image" content="/Icons/ATHSec-HomePageMeta.jpg">
-         
     </Head>
+    
   );
 }
 
