2bd8c850d4efecd2a0bf845128cb68d52d06d0b7
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-28 19:53:21 +0000

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,18 +3,15 @@ import Head from 'next/head'
 const Header = ({ title }) =>{
   return (
     <Head>
-      // <!-- Primary Meta Tags -->
       <meta name="title" content="ATHSec">
       <meta name="description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
 
-      // <!-- Open Graph / Facebook -->
       <meta property="og:type" content="website">
       <meta property="og:url" content="https://athsec.org/">
       <meta property="og:title" content="ATHSec">
       <meta property="og:description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
       <meta property="og:image" content="/Icons/ATHSec-HomePageMeta.jpg">
 
-      // <!-- Twitter -->
       <meta property="twitter:card" content="summary_large_image">
       <meta property="twitter:url" content="https://athsec.org/">
       <meta property="twitter:title" content="ATHSec">
