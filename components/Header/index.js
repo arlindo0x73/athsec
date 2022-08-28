dcc4a775dee0ee1063d046b16c206d1957ca1331
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-28 19:58:59 +0000

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,6 +3,15 @@ import Head from 'next/head'
 const Header = ({ title }) =>{
   return (
     <Head>
+    
+      <title>{title}</title>
+      <link rel="shortcut icon" href="/Icons/índice.jpg" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    
+    </Head>
+    
+    <Head>
+    
       <meta name="title" content="ATHSec">
       <meta name="description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
 
@@ -17,9 +26,7 @@ const Header = ({ title }) =>{
       <meta property="twitter:title" content="ATHSec">
       <meta property="twitter:description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
       <meta property="twitter:image" content="/Icons/ATHSec-HomePageMeta.jpg">
-    
-      <link rel="shortcut icon" href="/Icons/índice.jpg" />
-      <title>{title}</title>
+         
     </Head>
   );
 }
