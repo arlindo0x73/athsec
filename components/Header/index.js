0d8c2935fd08554c3e6590abd2cfc6e3ad0bea06
Arlindo Cossa Junior <52585849+SwagneyCod3@users.noreply.github.com>
2022-08-28 20:15:06 +0000

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,13 +3,10 @@ import Head from 'next/head'
 const Header = ({ title }) =>{
   return (
     <Head>
-    
       <title>{title}</title>
       <link rel="shortcut icon" href="/Icons/índice.jpg" />
       <meta name="description" content="ATHSec™ é uma empresa de defesa cibernética que fornece Avaliação de Segurança, Design de Arquitetura de Segurança e Detecção e Resposta Gerenciada">
-
     </Head>
-    
   );
 }
 
